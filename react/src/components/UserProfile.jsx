b8832ff6fca981acc7ed44075fef4126082b31d0
judiciousmurich <murichjudicious45@gmail.com>
2024-05-23 23:53:55 +0300

    feat:add files

--- react/src/components/UserProfile.jsx
+++ react/src/components/UserProfile.jsx
@@ -1,9 +1,10 @@
 import UserFavouriteFoods from "./UserFavouriteFoods";
+import UserUsername from "./UserUsername";
 
 export default function UserProfile () {
     return(
         <div id="user-profile">
-            
+            <UserUsername/>
             <div>
                 <span>Email:</span>
                 <span>murichjudicious@gmail.com</span>
