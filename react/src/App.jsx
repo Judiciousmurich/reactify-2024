1d510fad975b6656e109f0a065faf564764a4565
judiciousmurich <murichjudicious45@gmail.com>
2024-05-24 23:02:08 +0300

    feat:add files

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,18 @@
 import UserProfile from "./components/UserProfile";
 
 export default function App(){
+    const callMe = () => {
+        console,log("hello")
+    };
     return(
         <div>
             <h1>Root Component</h1>
         <UserProfile age= {21} isLoggedIn={true} favouriteFoods={[{
             name: "pizza",
         },
-        ]}/>
+        ]}
+        CallMe={callMe}/>
+      
         </div>
     )
 
