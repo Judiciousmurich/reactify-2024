affa0bb25d80d3a2a0a483e1b69a33d53b29e2e5
judiciousmurich <murichjudicious45@gmail.com>
2024-05-24 20:10:50 +0300

    feat:add tables values

--- react/src/components/UserUsername.jsx
+++ react/src/components/UserUsername.jsx
@@ -3,7 +3,7 @@ export default function UserUsername (props) {
     return (
         <div>
             <b>username:</b>
-            <span>props.username</span>
+            <span>{props.username}</span>
         </div>
     )
 
