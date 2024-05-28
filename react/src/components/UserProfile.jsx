30cefc231aa2821bb24847db4567613e5dedb0c5
judiciousmurich <murichjudicious45@gmail.com>
2024-05-28 10:44:57 +0300

    feat:add files

--- react/src/components/UserProfile.jsx
+++ react/src/components/UserProfile.jsx
@@ -7,7 +7,7 @@ export default function UserProfile (props) {
 
     return(
         <div id="user-profile">
-            <UserUsername username = />
+            <UserUsername username = {}/>
             
               <UserFavouriteFoods/> 
             <div>
