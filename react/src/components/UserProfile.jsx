3fb3fae54e2034cb1ea4d341c5cfee147ebf4860
judiciousmurich <murichjudicious45@gmail.com>
2024-05-28 11:52:10 +0300

    feat:add files

--- react/src/components/UserProfile.jsx
+++ react/src/components/UserProfile.jsx
@@ -1,38 +1,34 @@
-import propTypes from "prop-types"
+import propTypes from "prop-types";
 import UserFavouriteFoods from "./UserFavouriteFoods";
 import UserUsername from "./UserUsername";
 
-export default function UserProfile (props) {
-    console.log(props)
+export default function UserProfile(props) {
+  console.log(props);
 
-    return(
-       <>
+  return (
+    <>
+      <UserUsername username={props.username} />
 
-
-            <UserUsername username = {props.username}/>
-            
-              <UserFavouriteFoods/> 
-         
-                <span>Age:</span>
-                <span>{props.age}</span>
-            
-            
-    <UserFavouriteFoods/>
-{String(props.isLoggedIn)}
+      <UserFavouriteFoods />
+      <>
+        <b>Age:</b>
+        <span>{props.age}</span>
       </>
-        
-    )
+
+      <UserFavouriteFoods />
+      {String(props.isLoggedIn)}
+    </>
+  );
 }
 UserProfile.PropTypes = {
-    username:propTypes.string.isRequired,
-    age:propTypes.number.isRequired,
-    callMe: propTypes.number.isRequired,
-    isLoggedIn: propTypes.bool,
-    favouriteFoods: propTypes.arrayOf(
-        propTypes.shape({
-            name: propTypes.string.isRequired,
-         id: propTypes.number.isRequired,
-        })
-    )
-    
-}
+  username: propTypes.string.isRequired,
+  age: propTypes.number.isRequired,
+  callMe: propTypes.number.isRequired,
+  isLoggedIn: propTypes.bool,
+  favouriteFoods: propTypes.arrayOf(
+    propTypes.shape({
+      name: propTypes.string.isRequired,
+      id: propTypes.number.isRequired,
+    })
+  ),
+};
