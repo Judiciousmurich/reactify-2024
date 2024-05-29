ae04f2663f67ada8b9a73fc873233de2563c2e39
judiciousmurich <murichjudicious45@gmail.com>
2024-05-29 11:39:05 +0300

    feat:add files

--- react/src/components/UserUsername.jsx
+++ react/src/components/UserUsername.jsx
@@ -3,7 +3,7 @@ export default function UserUsername (props) {
     console.log(props.password)
     return (
         <div>
-            <b className="username">username:</b>
+            <b className="username ">username:</b>
             <span>{props.username}</span>
             <b>Password:</b>
             <span>{props.password}</span>
